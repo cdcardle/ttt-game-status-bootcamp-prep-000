8652aef575ec1ca4348d40ae9f15cf37dd1b3623
Chris Cardle <cdcardle@gmail.com>
2017-10-09 12:08:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won?(board)
       return win_com
     elsif (board[win_com[0]] == "O" && board[win_com[1]] == "O" && board[win_com[2]] == "O")
       return win_com
-    elsif board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    elsif board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       return nil
     elsif puts "hi"
     end
